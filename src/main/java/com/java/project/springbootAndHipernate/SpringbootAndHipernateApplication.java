9f69514d73bb0a2947e2265f4b61579a50f9be13
= <=>
2022-12-17 22:39:17 +0300

    begining

---
+++ src/main/java/com/java/project/springbootAndHipernate/SpringbootAndHipernateApplication.java
@@ -0,0 +1,13 @@
+package com.java.project.springbootAndHipernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAndHipernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAndHipernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/project/springbootAndHipernate/SpringbootAndHipernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.project.springbootAndHipernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAndHipernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
