0984e6d2b158023769976f1f74a886086404d343
= <=>
2022-12-18 14:18:55 +0300

    entitiy

---
+++ src/main/java/com/java/project/springbootAndHipernate/Entities/Actor.java
@@ -0,0 +1,48 @@
+package com.java.project.springbootAndHipernate.Entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="Actor")
+public class Actor {
+	@Id
+	@Column(name="actor_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	public Actor(int id, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+	
+	
+	
+
+}
